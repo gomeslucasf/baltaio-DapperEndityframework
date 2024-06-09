4e8fe99bd85e1e45a406bf1da09ad912a04b77ff
Lucas Gomes <gomesfeliplucas@gmail.com>
2024-06-09 14:22:58 -0300

    Aula de dapper

--- Models/User.cs
+++ Models/User.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using Dapper.Contrib.Extensions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace baltaio_DapperEndityframework.Models
 {
+    [Table("[User]")]
     public class User
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public string Slug { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,99 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using baltaio_DapperEndityframework.Models;
+using Dapper;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace baltaio_DapperEndityframework
+{
+    internal class Program
+    {
+        private const string _connection = @"Server=localhost,1433;Database=Blog;User ID=SA;Password=1q2w3e4r@#$;TrustServerCertificate=True";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser(new User { 
+            //    Name = "Felipe Silva",
+            //    Email = "Felip.silva@gmail.com",
+            //    PasswordHash = "asdf",
+            //    Bio = "Professor",
+            //    Image = "Textando",
+            //    Slug = "Felipe-Silva"
+            //}); 
+            //UpdateUser(new User
+            //{
+            //    Id = 3,
+            //    Name = "Lucas Silva",
+            //    Email = "Felip.silva@gmail.com",
+            //    PasswordHash = "asdf",
+            //    Bio = "Professor",
+            //    Image = "Textando",
+            //    Slug = "Felipe-Silva"
+            //});
+            DeleteUser(new User { Id = 3 });
+
+        }
+
+        public static void ReadUsers()
+        {
+            using (var connection = new SqlConnection(_connection)) {
+                var users = connection.GetAll<User>();
+
+                foreach (var user in users) {
+                    Console.WriteLine(user.Name);
+                }
+
+            }        
+        }
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(_connection))
+            {
+                var user = connection.Get<User>(1);
+
+                Console.WriteLine(user.Name);
+
+            }
+        }
+
+
+        public static void CreateUser(User usuario)
+        {
+            using (var connection = new SqlConnection(_connection))
+            {
+                var id = connection.Insert<User>(usuario);
+
+                var user = connection.Get<User>(id);
+
+                Console.WriteLine(user.Name);
+
+            }
+        }
+        public static void UpdateUser(User usuario)
+        {
+            using (var connection = new SqlConnection(_connection))
+            {
+                var id = connection.Update<User>(usuario);
+
+                var user = connection.Get<User>(usuario.Id);
+
+                Console.WriteLine(user.Name);
+
+            }
+        }
+        public static void DeleteUser(User usuario)
+        {
+            using (var connection = new SqlConnection(_connection))
+            {
+                var user = connection.Get<User>(usuario.Id);
+
+                var id = connection.Delete<User>(user);
+
+                Console.WriteLine($"Usuário {user.Name} excluído");
+
+            }
+        }
+    }
+}
